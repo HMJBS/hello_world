5b6b8032a5194f0f4d9c1b889ddc6ac71f1794bd
yohei yamamoto <yamarail.pc@live.jp>
2016-11-26 16:22:08 +0900

    add JS

---
+++ UDP_send.js
@@ -0,0 +1,8 @@
+var dgram = require('dgram');
+var messege = new Buffer("This is another successful procedure");
+var client = dgram.createSocket("udp4");
+client.send(messege,0,messege.length,4000,"192.168.11.23",
+    function(err){
+        client.close();
+    }
+);
